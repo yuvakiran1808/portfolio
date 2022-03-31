cdff610d002a0cf060754cf3ad8fc64ea67d7752
yuvakiran1808 <pyuvasaikiran@gmail.com>
2022-03-31 17:10:18 +0530

    contactform

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-var m = window.innerWidth;
-console.log(m);
-const navtoggle =document.querySelector(".navbar-toggler");
-const  navbar = document.querySelector(".rowheight")
+var topbtn = document.getElementById("topbtn");
+
+// When the user scrolls down 20px from the s of the document, show the button
+window.onscroll = ()=> {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbtn.style.display = "block";
+  } else {
+    topbtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
